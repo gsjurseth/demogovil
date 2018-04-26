1518882db82c60ff914f217fb643fecb0e996839
Geir Sjurseth <gsjurseth@google.com>
2018-04-26 13:40:56 +0000

    some minor updates

---
+++ sharedFlows/OauthValidation/sharedflowbundle/resources/jsc/JavaScriptExtract.AppAttributes.js
@@ -0,0 +1,8 @@
+ var daXml = AccessEntity.AccessEntity.AppAttributes.content.asXML;
+ 
+ print(daXml);
+ // /App/Attributes/Attribute[Name='officeNumber']/Value/text()
+ 
+ //context.setVariable('user.name', daXml.)
+ 
+ 
